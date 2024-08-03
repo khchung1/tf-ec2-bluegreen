2fa82bb548eb8b12836df34b45118729ffb0bef2
khchung1 <khchung87@gmail.com>
2024-08-03 11:12:12 +0800

    my first commit

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "app" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = [aws_security_group.alb.id]
     ipv6_cidr_blocks = ["::/0"]
   }
 
