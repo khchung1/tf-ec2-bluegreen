1257aa75bed44c1fd6a626e631dca5be41ab276f
khchung1 <khchung87@gmail.com>
2024-08-09 23:29:10 +0800

    bluegreen deplyment

--- main.tf
+++ main.tf
@@ -57,7 +57,6 @@ resource "aws_security_group" "app" {
     to_port          = 0
     protocol         = "-1"
     security_groups = [aws_security_group.alb.id]
-    ipv6_cidr_blocks = ["::/0"]
       
   }
 
@@ -66,7 +65,6 @@ resource "aws_security_group" "app" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
 }
@@ -87,21 +85,21 @@ resource "aws_lb_listener" "app" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.blue.arn
-    # forward {
-    #   target_group {
-    #     arn =     aws_lb_target_group.blue.arn
-    #     weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
-    #   }
+    forward {
+      target_group {
+        arn =     aws_lb_target_group.blue.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
+      }
 
-    #   target_group {
-    #     arn =    aws_lb_target_group.green.arn
-    #     weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
-    #   }
+      target_group {
+        arn =    aws_lb_target_group.green.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
+      }
 
-    #   stickiness {
-    #     enabled  = false
-    #     duration = 1
-    #   }
-    # }
+      stickiness {
+        enabled  = false
+        duration = 1
+      }
+    }
   }
 }
